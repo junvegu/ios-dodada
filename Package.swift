712fc567a29179d53f1be856e446097a4476d2cd
Junior <juniorquegu@gmail.com>
2024-03-27 13:33:56 -0500

    setup initial fonts colors and assets

--- Package.swift
+++ Package.swift
@@ -4,20 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "ios-Dodada",
+    name: "Dodada",
+    platforms: [
+        .iOS(.v15)
+       ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "ios-Dodada",
-            targets: ["ios-Dodada"]),
+            name: "Dodada",
+            targets: ["Dodada"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ios-Dodada"),
+            name: "Dodada"),
         .testTarget(
-            name: "ios-DodadaTests",
-            dependencies: ["ios-Dodada"]),
+            name: "DodadaTests",
+            dependencies: ["Dodada"]),
     ]
 )
---
+++ Sources/ios-Dodada/Delivery/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Junior Quevedo Gutiérrez  on 20/01/24.
+//
+
+import Foundation
