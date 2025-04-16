63a5b758dac32789bc9b36d54109c3ad52a2ca7f
Benjamin Contreras <benjacontreras.6000@gmail.com>
2025-04-16 02:13:49 -0500

    Creating the DDDMediaCard component

---
+++ Sources/Dodada/Delivery/Molecules/MediaCard/DDDMediaCard.swift
@@ -0,0 +1,71 @@
+//
+//  DDDMediaCard.swift
+//  Dodada
+//
+//  Created by Aly Benjamin Contreras Del Pino on 10/04/25.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+public struct DDDMediaCard: View {
+    let imageURL: URL?
+    let text: String
+    let views: String
+    let isLive: Bool
+    let author: String
+    
+    public var body: some View {
+        VStack (alignment: .leading){
+            
+            VStack{
+                VStack{
+                    HStack {
+                        DDDBadge(text: views, icon: .eye, type: .filled, state: .secondary)
+                        Spacer()
+                        if isLive{
+                            DDDBadge(text: "Live", icon: .zap, type: .filled, state: .default)
+                        }
+                    }
+                    .padding(.top, 8)
+                    .padding(.horizontal, 8)
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .leading){
+                        Text(text)
+                            .apply(token: .subheadline,
+                                   weight: .bold)
+                    }.foregroundColor(.white)
+                        .padding(.bottom, 8)
+                        .padding(.horizontal, 8)
+                    
+                }
+                .frame(width: 164,height: 192)
+                .background(
+                    WebImage(url: imageURL)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 164, height: 192)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                )
+                .padding(.trailing, 16)
+                .padding(.bottom, 16)
+            }
+            
+            VStack(alignment: .leading){
+                Text("TikTok")
+                    .apply(token: .caption2,
+                           weight: .bold)
+                Text(author)
+                    .apply(token: .subheadline,
+                           weight: .bold)
+            }
+        }
+        .frame(width: 164, height: 233)
+    }
+}
+
+#Preview {
+    DDDMediaCard(imageURL: URL(string: "https://www.biografia.de/biografia/Emma-Myers.jpg"), text: "Emma Myers, my love, her is so cute", views: "2.5k", isLive: false, author: "Fxkrzn")
+}
